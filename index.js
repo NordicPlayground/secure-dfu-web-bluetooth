1e8ae4ab0ca0cef404134c4d3836cce0915a4f21
Michael Dietz <Michael.Dietz@nordicsemi.no>
2016-10-25 13:28:45 +0200

    Lint code with eslint and Airbnb style guide. Get async tests working.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 
 const AdmZip = require('adm-zip');
@@ -7,33 +5,33 @@ const AdmZip = require('adm-zip');
 
 /**
  * Un-zips the compressed file and returns each entry contained in it.
- * @param {string} zip_file_path : The path to the compressed file to be un-zipped.
- * @return {array of strings} An array of strings corresponding to each entry contained in the compressed file.
+ * @param {string} zipFilePath : The path to the compressed file to be un-zipped.
+ * @return {array of strings} An array of strings corresponding to each entry in zipFilePath.
  */
-function unZip(zip_file_path) {
-    let zip = new AdmZip(zip_file_path);
-    const zipEntries = zip.getEntries();
+function unZip(zipFilePath) {
+  const zip = new AdmZip(zipFilePath);
+  const zipEntries = zip.getEntries();
 
-    zip.extractAllTo(`${__dirname}/tmp`, true);
+  zip.extractAllTo(`${__dirname}/tmp`, true);
 
-    let entryNames = [];
-    zipEntries.forEach(zipEntry => {
-        entryNames.push(zipEntry.entryName);
-    });
+  const entryNames = [];
+  zipEntries.forEach((zipEntry) => {
+    entryNames.push(zipEntry.entryName);
+  });
 
-    return entryNames;
-};
+  return entryNames;
+}
 
 
-function parseManifest(manifest_file_path, callback) {
-	fs.readFile(manifest_file_path, (err, data) => {
-		if (err) {
-			throw err;
-		}
-		callback(JSON.parse(data));
-	});
+function parseManifest(manifestFilePath, callback) {
+  fs.readFile(manifestFilePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    callback(JSON.parse(data));
+  });
 
-	return 0;
+  return 0;
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
-'use strict';
-
 const fs = require('fs');
 
+const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 const unZip = require('../index').unZip;
@@ -9,16 +8,18 @@ const parseManifest = require('../index').parseManifest;
 
 
 describe('#unZip', () => {
-    it('should un-zip test_resources/dfu_test_app_hrm_s132.zip and print it\'s contents.', function() {
-        const result = unZip(`${__dirname}/test_resources/dfu_test_app_hrm_s132.zip`);
-        expect(fs.lstatSync(`${__dirname}/../tmp`).isDirectory()).to.equal(true);
-        expect(result.toString()).to.equal(['manifest.json', 'nrf52832_xxaa.bin', 'nrf52832_xxaa.dat'].toString())
+  it('should un-zip test_resources/dfu_test_app_hrm_s132.zip and print it\'s contents.', () => {
+    const result = unZip(`${__dirname}/test_resources/dfu_test_app_hrm_s132.zip`);
+    expect(fs.lstatSync(`${__dirname}/../tmp`).isDirectory()).to.equal(true);
+    expect(result.toString()).to.equal(['manifest.json', 'nrf52832_xxaa.bin', 'nrf52832_xxaa.dat'].toString());
+  });
+
+  it('should parse the manifest.json file passed to it and return a json object.', (done) => {
+    const errCode = parseManifest(`${__dirname}/../tmp/manifest.json`, (result) => {
+      expect(result.toString()).to.equal({ manifest: { application: { bin_file: 'nrf52832_xxaa.bin', dat_file: 'nrf52832_xxaa.dat' } } }.toString());
+      done();
     });
 
-    it('should parse the manifest.json file passed to it and return a json object.', function() {
-        const errCode = parseManifest(`${__dirname}/../tmp/manifest.json`, (result) => {
-            expect(result).to.equal({ manifest: { application: { bin_file: 'nrf52832_xxaa.bin', dat_file: 'nrf52832_xxaa.dat' } } })
-        });
-        expect(errCode).to.equal(0)
-    });
+    expect(errCode).to.equal(0);
+  });
 });
