e7336d5ca359379a40cee57aef559a96a59fdbb1
Michael Dietz <Michael.Dietz@nordicsemi.no>
2016-10-25 13:47:23 +0200

    Add UUIDs for DFU service and characteristics.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const AdmZip = require('adm-zip');
 
 
+// https://infocenter.nordicsemi.com/topic/com.nordic.infocenter.sdk5.v12.0.0/lib_dfu_transport_ble.html?cp=4_0_0_3_4_3_2
+const NORDIC_SEMI_BASE_UUID = '8EC90001-xxxx-4F60-9FB8-838830DAEA50';
+const SECURE_DFU_SERVICE_UUID = NORDIC_SEMI_BASE_UUID.replace('xxxx', 'FE59');
+const DFU_CONTROL_POINT_UUID = NORDIC_SEMI_BASE_UUID.replace('xxxx', '0001');
+const DFU_PACKET_UUID = NORDIC_SEMI_BASE_UUID.replace('xxxx', '0002');
+
+
 /**
  * Un-zips the compressed file and returns each entry contained in it.
  * @param {string} zipFilePath : The path to the compressed file to be un-zipped.
@@ -23,6 +30,13 @@ function unZip(zipFilePath) {
 }
 
 
+/**
+ * See: https://github.com/NordicSemiconductor/pc-nrfutil/blob/master/nordicsemi/dfu/package.py#L79
+        for a description of the manifest file.
+ * @param {string} manifestFilePath : The path to the manifest file to be parsed.
+ * @param {function} callback : Callback that will be fired with parsed data after asyn completion.
+ * @return {int} Error code, 0 means SUCCESS.
+ */
 function parseManifest(manifestFilePath, callback) {
   fs.readFile(manifestFilePath, (err, data) => {
     if (err) {
--- test/test.js
+++ test/test.js
@@ -10,8 +10,9 @@ const parseManifest = require('../index').parseManifest;
 describe('#unZip', () => {
   it('should un-zip test_resources/dfu_test_app_hrm_s132.zip and print it\'s contents.', () => {
     const result = unZip(`${__dirname}/test_resources/dfu_test_app_hrm_s132.zip`);
-    expect(fs.lstatSync(`${__dirname}/../tmp`).isDirectory()).to.equal(true);
     expect(result.toString()).to.equal(['manifest.json', 'nrf52832_xxaa.bin', 'nrf52832_xxaa.dat'].toString());
+
+    expect(fs.lstatSync(`${__dirname}/../tmp`).isDirectory()).to.equal(true);
   });
 
   it('should parse the manifest.json file passed to it and return a json object.', (done) => {
