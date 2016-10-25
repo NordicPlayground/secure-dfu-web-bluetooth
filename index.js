8a31b6a97560e5e963b63a12db9dd051898b2844
Michael Dietz <Michael.Dietz@nordicsemi.no>
2016-10-25 12:45:06 +0200

    Add function to un-zip compressed file and unit test.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const AdmZip = require('adm-zip');
+
+
 /**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
+ * Un-zips the compressed file and returns each entry contained in it.
+ * @param {string} zip_file_path : The path to the compressed file to be un-zipped.
+ * @return {array of strings} An array of strings corresponding to each entry contained in the compressed file.
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+function unZip(zip_file_path) {
+    let zip = new AdmZip(zip_file_path);
+    const zipEntries = zip.getEntries();
+
+    zip.extractAllTo(`${__dirname}/tmp`, true);
+
+    let entryNames = [];
+    zipEntries.forEach(zipEntry => {
+        entryNames.push(zipEntry.entryName);
+    });
+
+    return entryNames;
 };
+
+
+exports.unZip = unZip;
--- test/test.js
+++ test/test.js
@@ -1,46 +1,15 @@
 'use strict';
 
-var expect = require('chai').expect;
-var numFormatter = require('../index');
+const fs = require('fs');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
-    });
+const expect = require('chai').expect;
+const unZip = require('../index').unZip;
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
-    });
 
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
-
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
-
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
-
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
-
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
-
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
+describe('#unZip', () => {
+    it('should un-zip test_resources/dfu_test_app_hrm_s132.zip and print it\'s contents.', function() {
+        const result = unZip(`${__dirname}/test_resources/dfu_test_app_hrm_s132.zip`);
+        expect(fs.lstatSync(`${__dirname}/../tmp`).isDirectory()).to.equal(true);
+        expect(result.toString()).to.equal(['manifest.json', 'nrf52832_xxaa.bin', 'nrf52832_xxaa.dat'].toString())
     });
 });
